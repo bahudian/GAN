fbb2c12754e9c00649a04c6a387ed868782a8adc
Vatche <vbahudian@gmail.com>
2023-04-14 05:16:42 -0700

    added python script

---
+++ mlbgan.py
@@ -0,0 +1,59 @@
+# HIDE OUTPUT
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Activation
+from tensorflow.keras.callbacks import EarlyStopping
+from sklearn.model_selection import train_test_split
+import pandas as pd
+import io
+import os
+import requests
+import numpy as np
+from sklearn import metrics
+
+df = pd.read_csv(
+    "https://www.vatche.appboxes.co/mlb2023.csv")
+
+
+COLS_USED = ['Age','G','PA','AB','R','H','2B','3B','HR','RBI','SB','CS','BB','SO','BA','OBP','SLG']
+
+COLS_TRAIN = ['Age','G','PA','AB','H','2B','3B','HR','RBI','SB','CS','BB','SO','BA','OBP','SLG'] 
+
+
+df = df[COLS_USED]
+
+
+# Split into training and test sets
+df_x_train, df_x_test, df_y_train, df_y_test = train_test_split(
+    df.drop("R", axis=1),
+    df["R"],
+    test_size=0.20,
+    #shuffle=False,
+    random_state=42,
+)
+
+# Create dataframe versions for tabular GAN
+df_x_test, df_y_test = df_x_test.reset_index(drop=True), \
+  df_y_test.reset_index(drop=True)
+df_y_train = pd.DataFrame(df_y_train)
+df_y_test = pd.DataFrame(df_y_test)
+
+# Pandas to Numpy
+x_train = df_x_train.values
+x_test = df_x_test.values
+y_train = df_y_train.values
+y_test = df_y_test.values
+
+# Build the neural network
+model = Sequential()
+# Hidden 1
+model.add(Dense(50, input_dim=x_train.shape[1], activation='relu')) 
+model.add(Dense(25, activation='relu')) # Hidden 2
+model.add(Dense(12, activation='relu')) # Hidden 2
+model.add(Dense(1)) # Output
+model.compile(loss='mean_squared_error', optimizer='adam')
+
+monitor = EarlyStopping(monitor='val_loss', min_delta=1e-3, 
+        patience=5, verbose=1, mode='auto',
+        restore_best_weights=True)
+model.fit(x_train,y_train,validation_data=(x_test,y_test),
+        callbacks=[monitor], verbose=2,epochs=1000)
